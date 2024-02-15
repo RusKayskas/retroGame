bfefb4182ac522d6bd908b17047ec88ed017583a
RusKayskas <KayskasRus@yandex.ru>
2024-02-15 03:14:32 +0300

    init script scructure

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log('1');
+//игрок 1 создаем классы и структуру  этап 1
+class player {
+
+}
+
+//тут создание снарядов и создание сетки и тд
+class Projectile {
+  
+}
+
+class Enemy {
+
+}
+
+//управление всей игрой
+class Game {
+
+}
+
+document.addEventListener('load', function() {
+  //обьявляем переменные для работы в canvas этап 1
+  const canvas = document.getElementById('canvas_1');
+  const ctx = canvas.getContext('2d');
+  canvas.width = 600;
+  canvas.height = 800;
+  
+})
